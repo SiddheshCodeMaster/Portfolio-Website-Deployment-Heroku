48ce54bae50a0f6dfe46dada4ab2a1886e3c388e
siddheshshankargithub <siddhesh.shankar@github.com>
2022-01-13 10:13:30 +0530

    siddhesh folder Commit

---
+++ siddhesh/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from .models import Home,About,Portfolio,Category,Skills,Profile
+# Create your views here.
+
+def index(request):
+
+    # Home:
+    home = Home.objects.latest('updated')
+    
+   
+
+    # About:
+    about = About.objects.latest('updated')
+    profiles = Profile.objects.filter(about=about)
+
+    # Skills:
+    categories = Category.objects.all()
+
+    # Portfolio:
+    portfolios = Portfolio.objects.all()
+
+
+    context = {
+        'home':home,
+        'about':about,
+        'profiles': profiles,
+        'categories':categories,
+        'portfolios': portfolios
+    }
+
+    return render(request, 'index.html', context)
